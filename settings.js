5d075b5615085f1c833850d249b818f461e6b332
[ᴘʀᴀʙᴜᴅᴅʜᴀ] <102649352+sangithprabuwa@users.noreply.github.com>
2022-09-22 17:39:32 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,138 @@
+//PRABUDDHA OFFICIAL TEAM◎◎◎ https://github.com/PRABUDDHA-OFFICIAL\\
+//══════════════THANKS FOR CHEEMS BOT MD (DGNEON)═ALL THE CREADITS GOES TO HIM═════════════\\
+//GOOD LUCK ALL\\
+const fs = require('fs')
+const chalk = require('chalk')
+
+//API WEBSITE \\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//API KEY\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'your key',
+}
+
+
+//AUTO FUNCTIONS ( 📵 DONT EDIT 📵 ) ITS FOR NEXT UPDATE
+
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autobio = true  // auto bio system checking on Developing Leval
+global.autoRecord = true //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//VIDEO MENU 
+global.vidmenu = fs.readFileSync("./LocalData/theme/cyber.mp4") //gif and video menu
+
+//DOCUMENT VARIANTS
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//OWNER CONTACT
+global.vcardowner = ['94774155960'] //ur owner number
+global.ownername = "ᴘʀᴀʙᴜᴅᴅʜᴀ ᴏꜰꜰɪᴄɪᴀʟ" //ur owner name
+global.ytname = "YT: PRABUDDHA-OFFICIAL " //ur yt chanel name
+global.socialm = "GitHub: PRABUDDHA-OFFICIAL " //ur github or insta name
+global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+
+//BOT COSTOMIZEDTATION
+global.owner = ['94774155960']
+global.ownertag = '94774155960' //ur tag number
+global.botnumber = '94774155960' //Add your Bot Number For Log
+global.botname = 'PRABUDDHA OFFICIAL ' //ur bot name
+global.ig = "https://youtube.com/channel/UCtvPG7V0jVLPNFsdD1z9XvQ" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtube.com/channel/UCtvPG7V0jVLPNFsdD1z9XvQ" //ur website to be displayed
+global.sc = 'https://github.com/PRABUDDHA-OFFICIAL ' //script link
+global.linkz = 'https://youtube.com/channel/UCtvPG7V0jVLPNFsdD1z9XvQ
+global.reactmoji = "🎭" //ur menu react emoji
+global.themeemoji = "💫" //ur theme emoji
+global.packname = "PRABUDDHA OFFICIAL " //ur sticker watermark packname
+global.author = "©2022 PRABUDDHA" //ur sticker watermark author
+global.footer = "®ᴘʀᴀʙᴜᴅᴅʜᴀ ᴏꜰꜰɪᴄɪᴀʟ ᴡᴀʙᴏᴛ"
+global.aliveimg ="https://i.imgur.com/SdZFgwU.jpeg"
+global.allmenuimg ="https://i.imgur.com/SdZFgwU.jpeg"
+global.welcomeimg= "https://i.imgur.com/SdZFgwU.jpeg"
+global.gbyeimg = "https://i.imgur.com/SdZFgwU.jpeg"
+global.alivemsg = "PRABUDDHA OFFICIAL CREW. HEY THERE BOT ONLINE NOW. ©2022 PRABUDDHA®"
+
+//BOT MEDIAS
+global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur error pic
+global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
+
+//DATA BASED
+
+global.premium = ['94774155960'] //ADD YOU NUMBER
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//OTHERS
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.']
+global.sp = '⭔'
+global.mess = {
+    success: ' _🎭Done_',
+    admin: ' _💫This Feature Is Only For Admin🎭!_',
+    botAdmin: '💫 _Bot Must Be Admin First!_',
+    owner: '💫 _This Feature Is Only For Owner!🎭_',
+    group: '💫 _Feature Used Only For Groups!🎭_',
+    private: '💫_Features Used Only For Private Chat!🎭_',
+    bot: '💫 _This Feature Is Only For Bot🎭_',
+    wait: '💫 _Processing Please Wait🎭..._',
+    linkm: '♻_Where is the link?💦_',
+    error: '```Error♻!!```',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./LocalData/theme/cyber.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
